16635bd08410aa7c0c2495422303e4c1202dd793
franklinVillegas <091236villegas@gmail.com>
2024-02-18 02:08:45 -0500

    model de detalle propuesta

---
+++ app/Models/DetallePropuesta.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetallePropuesta extends Model
+{
+    use HasFactory;
+    protected $table = 'detalle_propuesta';
+    protected $fillable = [
+        'id',
+        'descripcion',
+        'cantidad',
+        'precio_unitario',
+        'precio_total',
+        'id_propuesta',
+        'created_at',
+        'updated_at',
+
+    ];
+
+    public function cotizacion(){
+        return $this->belongsTo(cotizacionPropuesta::class, 'id_cotizacion', 'id');
+    }    
+}
